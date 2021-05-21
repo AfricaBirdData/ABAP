d4e30ad44a071a81fa996d7a4c8c6dc8d7d5e1c6
patchcervan <francerper@gmail.com>
2021-05-21 16:51:57 +0200

    Fixed merge

--- R/searchSabapSpecies.R
+++ R/searchSabapSpecies.R
@@ -14,33 +14,12 @@ searchSabapSpecies <- function(species){
 
   url <- paste0("http://api.adu.org.za/sabap2/v2/search/species/", species)
 
-<<<<<<< HEAD
   # Extract data
   myfile <- RCurl::getURL(url, ssl.verifyhost = FALSE, ssl.verifypeer = FALSE)
   jsonfile <- rjson::fromJSON(myfile)
 
   # Reformat
   out <- SABAP::jsonToTibble(jsonfile$data)
-=======
-  # Read in data
-  myfile <- RCurl::getURL(url, ssl.verifyhost = FALSE, ssl.verifypeer = FALSE)
-
-  jsonfile <- rjson::fromJSON(myfile)
-
-  out <- jsonfile$data %>%
-    lapply(function(x) {
-      x[sapply(x, is.null)] <- NA
-      x <- unlist(x)
-      x[x == "N/A"] <- NA
-      cnames <- names(x)
-      df <- data.frame()
-      df <- rbind(df, x)
-      names(df) <- cnames
-      return(df)
-    }) %>%
-    data.table::rbindlist(fill = TRUE) %>%
-    dplyr::as_tibble()
->>>>>>> fccea4bcdf3fe760048e52025e63874656682e29
 
   return(out)
 }
