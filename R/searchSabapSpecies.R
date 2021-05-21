7d239c110c697c789465ca9d5dbc5d51d6eecb3d
patchcervan <francerper@gmail.com>
2021-05-21 12:59:22 +0200

    Created searchSabapSpecies and jsonToTibble

---
+++ R/searchSabapSpecies.R
@@ -0,0 +1,25 @@
+
+#' Search species in SABAP2 data base
+#'
+#' @param species A character string with any part of the common or taxonomic name, or, the SABAP2 species code
+#'
+#' @return A tibble with taxonomic information and SABAP2 codes of all species matching the search
+#' @export
+#'
+#' @examples
+#' searchSabapSpecies("Duck")
+#' searchSabapSpecies(169)
+#' searchSabapSpecies("Fulvous")
+searchSabapSpecies <- function(species){
+
+  url <- paste0("http://api.adu.org.za/sabap2/v2/search/species/", species)
+
+  # Extract data
+  myfile <- RCurl::getURL(url, ssl.verifyhost = FALSE, ssl.verifypeer = FALSE)
+  jsonfile <- rjson::fromJSON(myfile)
+
+  # Reformat
+  out <- SABAP::jsonToTibble(jsonfile$data)
+
+  return(out)
+}
