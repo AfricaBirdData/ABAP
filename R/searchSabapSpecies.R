9602dc31ed4830c6c097775eebdc7237d84937dc
patchcervan <francerper@gmail.com>
2021-10-15 16:46:19 +0200

    New API when passing a species code to searchSabapSpecies

--- R/searchSabapSpecies.R
+++ R/searchSabapSpecies.R
@@ -1,9 +1,14 @@
 
 #' Search species in SABAP2 data base
 #'
-#' @param species A character string with any part of the common or taxonomic name, or, the SABAP2 species code. Note that full names will not work; choose a single word.
+#' @param species A character string with any part of the common or taxonomic
+#' name, or, the SABAP2 species code (SAFRING code). Note that full names will
+#' not work; choose a single word.
 #'
-#' @return A tibble with taxonomic information and SABAP2 codes of all species matching the search
+#' @return A tibble with taxonomic information and SABAP2 codes of all species
+#' matching the search. At the moment, if a species code is passed, the new API
+#' is used. For convenience functionality for entering a common name is
+#' maintained through the old API, but this might be discontinued.
 #' @export
 #'
 #' @examples
@@ -12,7 +17,14 @@
 #' searchSabapSpecies("Fulvous")
 searchSabapSpecies <- function(species){
 
-  url <- paste0("http://api.adu.org.za/sabap2/v2/search/species/", species)
+  # test if species contains letters as opposed to only numbers
+  old <- grepl("[a-z]", species, ignore.case = TRUE)
+
+  if(old){
+    url <- paste0("http://api.adu.org.za/sabap2/v2/search/species/", species)
+  } else {
+    url <- paste0("http://api.birdmap.africa/sabap2/v2/species/info/", species, "?curt=1")
+  }
 
   # Extract data
   myfile <- RCurl::getURL(url, ssl.verifyhost = FALSE, ssl.verifypeer = FALSE)
@@ -20,6 +32,10 @@ searchSabapSpecies <- function(species){
 
   # Reformat
   out <- SABAP::jsonToTibble(jsonfile$data)
+
+  if(old){
+    names(out)[1] <- "SAFRING_No"
+  }
 
   return(out)
 }
