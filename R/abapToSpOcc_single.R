343c72ca4a382c514d7df94694955bf230975f03
Dominic Henry <dominichenry@gmail.com>
2022-02-25 18:14:30 -0500

    Updated function documentation

--- R/abapToSpOcc_single.R
+++ R/abapToSpOcc_single.R
@@ -1,25 +1,37 @@
-#' ABAP to spOccupancy single species
+#' ABAP to spOccupancy (single-species)
 #'
-#' @description Transforms a raw ABAP data frame into a list formatted for use in a single-species spOccupancy model
+#' @description This function transforms a raw ABAP data frame (returned by \code{\link{getAbapData}}) into an list which can be used to fit single-species occupancy models using \code{\link[spOccupancy]{spPGOcc}}(spatial) or \code{\link[spOccupancy]{PGOcc}}(non-spatial). The `spOccupancy` package fits single-species, multi-species, and integrated non-spatial and spatial occupancy models using Markov Chain Monte Carlo (MCMC).
 #'
-#' @param abap_data ABAP data downloaded using \code{\link{getAbapData}}
-#' @param pentads sf object returned by \code{\link{getRegionPentads}}. Defaults to `NULL`.
+#' @param abap_data single-season ABAP data downloaded using \code{\link{getAbapData}}.
+#' @param pentads an sf object returned by \code{\link{getRegionPentads}}. Defaults to `NULL`.
 #'
-#' @note If pentads are specified then output can be used in `spOccupancy::spPGOcc`. If no spatial data are provided the output can be used in `spOccupancy::spPGOcc`
-#' @return A list containing data necessary for model fitting in `spOccupancy`. List elements are `y`,  `det.covs`, and `coords` (if pentad data supplied)
+#' @return A list containing data necessary for model fitting in `spOccupancy`. List elements are `y` (detection/non-detection data), `det.covs` (survey-level covariates), and `coords` (x and y centroids of pentads if spatial data are supplied).
+#'
+#' @details The \code{\link[spOccupancy]{spPGOcc}} function takes `coords` as an argument with X and Y coordinates included for each site. Within the context of ABAP these `coords` are the centroid of each sampling pentad. In order to provide these spatial data to this function, simply use \code{\link{getRegionPentads}} and provide the same inputs for `.region_type` and `.region` that are specified in corresponding \code{\link{getAbapData}} call.\cr
+#'
+#' If `pentads` are specified then the output can be automatically used in \code{\link[spOccupancy]{spPGOcc}}. If no spatial data are provided the output can be used in \code{\link[spOccupancy]{PGOcc}}.\cr
+#'
+#' In addition to reformatting the detection/non-detection ABAP data for use in `spOccupancy` occupancy models, this function also extracts two survey-level covariates and adds them to the output list: ` hours` and `jday`. The `hours` variable is the total number of hours spent atlassing which is recorded on the pentad card and `jday` is the Julian day corresponding to the first day of atlassing for that card.
+#' @author Dominic Henry <dominic.henry@gmail.com> \cr
+#' Pachi Cervantes
+#' @seealso \code{\link[spOccupancy]{spPGOcc}}, \code{\link[spOccupancy]{PGOcc}}
 #' @export
 #'
 #' @examples
-#' abap_single <- getAbapData(.spp_code = 212, .region_type = "province",
-#'                            .region = "Eastern Cape", .years = 2012)
+#' abap_data <- getAbapData(.spp_code = 212,
+#'                          .region_type = "province",
+#'                          .region = "Eastern Cape",
+#'                          .years = 2012)
 #'
 #' abap_pentads <- getRegionPentads(.region_type = "province",
 #'                                  .region = "Eastern Cape")
-#' ## List for spatial model
-#' abapToSpOcc_single(abap_single, abap_pentads)
 #'
-#' ## List for non-spatial model
-#' abapToSpOcc_single(abap_single)
+#'
+#' ## Return list for spatial occupancy model
+#' abapToSpOcc_single(abap_data, abap_pentads)
+#'
+#' ## List for non-spatial occupancy model
+#' abapToSpOcc_single(abap_data)
 abapToSpOcc_single <- function(abap_data, pentads = NULL){
 
     pentad_id <- unique(abap_data$Pentad)
