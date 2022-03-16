772de0c47b8a1f0e24e125bc97cd7f3e4946f56f
Dominic Henry <dominichenry@gmail.com>
2022-03-16 16:44:30 -0400

    Added functionality so that XY coords can be added as site covs.

--- R/abapToUnmarked_single.R
+++ R/abapToUnmarked_single.R
@@ -6,10 +6,13 @@
 #'
 #'
 #' @param abap_data single-season ABAP data downloaded using \code{\link{getAbapData}}
+#' @param pentads an `sf` object returned by \code{\link{getRegionPentads}}. Defaults to `NULL`.
 #'
-#' @details In addition to reformatting the detection/non-detection ABAP data for use in `unmarked`  and `ubms` occupancy models, this function also extracts two survey-level covariates: ` hours` and `jday`. The `hours` variable is the total number of hours spent atlassing which is recorded on the pentad card and `jday` is the Julian day corresponding to the first day of atlassing for that card.
+#' @details In addition to reformatting the detection/non-detection ABAP data for use in `unmarked`  and `ubms` occupancy models, this function also extracts two survey-level covariates: ` hours` and `jday`. The `hours` variable is the total number of hours spent atlassing which is recorded on the pentad card and `jday` is the Julian day corresponding to the first day of atlassing for that card. If `pentads` are provided the `unmarked` frame will add the x and y coordinates as site covariates (`siteCovs`). The spatial data can be used as random effects to run a restricted spatial regression (RSR) occupancy model in `ubms`. See \code{\link[ubms]{RSR}} for more details.
 #'
 #' @return an object of class  \code{\link[unmarked]{unmarkedFrameOccu}}
+#'
+#' @note It's important to make sure that the `.region_type` and `.region` are the same when extracting `abap_data` and `pentads` (see example below).
 #'
 #' @seealso \code{\link[unmarked]{occu}}, \code{\link[unmarked]{occuRN}}, \code{\link[ubms]{stan_occu}}, \code{\link[ubms]{stan_occuRN}}
 #'
@@ -29,8 +32,16 @@
 #'                            .region = "Eastern Cape",
 #'                            .years = 2012)
 #'
+#' abap_pentads <- getRegionPentads(.region_type = "province",
+#'                                  .region = "Eastern Cape")
+#'
+#' ## Return unmarked frame no site covariates
 #' um_df <- abapToUnmarked_single(abap_single)
-abapToUnmarked_single <- function(abap_data){
+#'
+#' ## Return unmarked frame with Pentad coordinates as site covariates
+#' um_df <- abapToUnmarked_single(abap_single, abap_pentads)
+#'
+abapToUnmarked_single <- function(abap_data, pentads = NULL){
 
     pentad_id <- unique(abap_data$Pentad)
 
@@ -40,6 +51,18 @@ abapToUnmarked_single <- function(abap_data){
         dplyr::count(Pentad) %>%
         dplyr::pull(n) %>%
         max()
+
+    ## Extract spatial data
+    if(!is.null(pentads)){
+
+        pentad_xy <- pentads %>%
+            dplyr::filter(pentad %in% pentad_id) %>%
+            sf::st_centroid() %>%
+            sf::st_coordinates() %>%
+            as.data.frame() %>%
+            dplyr::rename_all(tolower)
+
+    }
 
     ## Create empty arrays
     Y <-  array(NA, dim = c(n_sites, max_visits),
@@ -83,9 +106,19 @@ abapToUnmarked_single <- function(abap_data){
                      jday = as.data.frame(obs_jday))
 
     ## Create unmarked data frame
-    umf_abap <- unmarked::unmarkedFrameOccu(y = Y,
-                                            obsCovs = obs_covs,
-                                            mapInfo = NULL)
+    if(!is.null(pentads)){
+
+        umf_abap <- unmarked::unmarkedFrameOccu(y = Y,
+                                                siteCovs = pentad_xy,
+                                                obsCovs = obs_covs,
+                                                mapInfo = NULL)
+
+    } else if (is.null(pentads)){
+
+        umf_abap <- unmarked::unmarkedFrameOccu(y = Y,
+                                                obsCovs = obs_covs,
+                                                mapInfo = NULL)
+    }
 
     return(umf_abap)
 }
