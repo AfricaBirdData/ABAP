046ded36145a19fcd7ee7d6c4908758431bec7a5
patchcervan <francerper@gmail.com>
2022-06-08 18:28:35 +0200

    addEEtoUnmarked_single now takes vector of variable names

--- R/addEEtoUnmarked_single.R
+++ R/addEEtoUnmarked_single.R
@@ -4,7 +4,9 @@
 #'
 #' @param umf a single-season Unmarked frame containing ABAP detection/non-detection data returned by \code{\link{abapToUnmarked_single}}.
 #' @param ee_data a data frame with GEE data extracted using \code{\link{addVarEEcollection}} or \code{\link{addVarEEimage}}.
-#' @param ee_name a user-defined character string giving the desired name of the GEE site covariate.
+#' @param ee_names a user-defined character vector giving the names of the GEE site covariates
+#' we want to transfer. If not provided a wild guess will be made based on what variables are
+#' expected to be on an ABAP dataframe.
 #'
 #' @return an object of class \code{\link[unmarked]{unmarkedFrameOccu}} with survey and site covariates.
 #'
@@ -68,51 +70,48 @@
 #' ## Add to NDVI covariate to unmarked frame
 #' umf_single_ee <- addEEtoUnmarked_single(umf = umf_single,
 #'                                        ee_data = ndvi_mean,
-#'                                        ee_name = "NDVI_mean")
+#'                                        ee_names = "NDVI_mean")
 #' summary(umf_single_ee)
 #'
 #' ## Add surface water covariate to unmarked frame
 #' umf_single_ee <- addEEtoUnmarked_single(umf = umf_single,
 #'                                        ee_data = water_mean,
-#'                                        ee_name = "water_mean")
+#'                                        ee_names = "water_mean")
 #' summary(umf_single_ee)
 #'
-#' ## Add both covariates simultaneously using the pipe operator
-#' umf_single_ee <- umf_single %>%
-#'    addEEtoUnmarked_single(., ndvi_mean, "NDVI_mean") %>%
-#'    addEEtoUnmarked_single(., water_mean, "water_mean")
+#' ## Add both covariates simultaneously
+#' umf_single_ee <- addEEtoUnmarked_single(umf = umf_single,
+#'                                        ee_data = water_mean,
+#'                                        ee_names = c("water_mean", "NDVI_mean")
 #'
 #' summary(umf_single_ee)
 
 #' }
 
-addEEtoUnmarked_single <- function(umf, ee_data, ee_name) {
+addEEtoUnmarked_single <- function(umf, ee_data, ee_names = NULL) {
 
-    reducer_names <- c(
-        "mean", "max", "min", "count", "median",
-        "sum", "stdDev", "variance"
-    )
+    ee_data <- sf::st_drop_geometry(ee_data)
 
     builtin_names <- c(
         "id", "Name", "country", "full_qdgc",
-        "pentad", "province", "qdgc", "geometry"
+        "pentad", "province", "qdgc"
     )
 
-    cols_keep <- reducer_names[reducer_names %in% names(ee_data)]
-
-    if (length(cols_keep) == 0) {
+    if (is.null(ee_names)) {
         cols_keep <- names(ee_data)[!names(ee_data) %in% builtin_names]
+    } else {
+        cols_keep <- ee_names
     }
 
     umf_pentads <- dimnames(umf@y)[[1]]
 
     site_cov <- ee_data %>%
-        sf::st_drop_geometry() %>%
         dplyr::filter(pentad %in% umf_pentads) %>%
         dplyr::arrange(match(pentad, umf_pentads)) %>%
-        dplyr::select(dplyr::all_of(cols_keep)) %>%
-        dplyr::rename_with(.cols = 1, ~ {{ ee_name }})
+        dplyr::select(dplyr::all_of(cols_keep))
 
+    # Remove metadata from GEE download
+    attr(site_cov, "metadata") <- NULL
 
     if (is.null(umf@siteCovs)) {
         umf@siteCovs <- site_cov
