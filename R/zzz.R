01e54ad0c534e31e907346178f1f2470ca16081d
patchcervan <francerper@gmail.com>
2022-08-11 16:40:07 +0200

    Fixed startup message again

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 
     packageStartupMessage(
         strwrap(prefix = " ", width = 70,
-                "NOTE: in ABAP version 0.1 we have changed the naming convention
+                "NOTE: in ABAP version 0.1.0 we have changed the naming convention
                 for variables coming out of Google Earth Engine-related functions.
                 Previous code might not work as expected.")
     )
