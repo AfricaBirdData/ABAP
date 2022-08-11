07e2b6918b476e79a53215b45c15f77bcf8953d9
patchcervan <francerper@gmail.com>
2022-08-11 16:13:20 +0200

    Added warning message about GEE variables

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onLoad <- function(libname, pkgname) {
+
+    packageStartupMessage("NOTE: in ABAP version 0.1, we have changed the naming convention for variables coming out of
+                          Google Earth Engine-related functions. Previous code might not work as expected.")
+
+}
