392b4ba373a962af193d9e026460985fd1447a48
patchcervan <francerper@gmail.com>
2022-08-11 16:28:46 +0200

    Fixed startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,10 @@
 .onLoad <- function(libname, pkgname) {
 
-    packageStartupMessage("NOTE: in ABAP version 0.1, we have changed the naming convention for variables coming out of
-                          Google Earth Engine-related functions. Previous code might not work as expected.")
+    packageStartupMessage(
+        strwrap(prefix = " ", width = 70,
+                "NOTE: in ABAP version 0.1 we have changed the naming convention
+                for variables coming out of Google Earth Engine-related functions.
+                Previous code might not work as expected.")
+    )
 
 }
